3760c444a44c8afabcb763457e8a7751e25af561
RJ993 <196080894+RJ993@users.noreply.github.com>
2025-05-14 11:49:27 -0400

    Add new game and load game options

--- main.rb
+++ main.rb
@@ -2,10 +2,20 @@ require_relative 'lib/game-mechanics'
 require_relative 'lib/game-elements/secret_word'
 require_relative 'lib/game-elements/player'
 
-image = Image.new
-word_storage = SecretWord.new
-word_storage.generate_word
-puts "What is your name, Player?"
-player = Player.new(gets.chomp)
-game = Game.new(word_storage.word, player, player.name, image)
-game.play
+option = ''
+puts 'Hello, Player, welcome to Hangman!!! Please type "new" for new game or "load" to load a previous save.'
+until option == 'new' || option == 'load'
+  option = gets.chomp.downcase
+end
+if option == 'new'
+  image = Image.new
+  word_storage = SecretWord.new
+  word_storage.generate_word
+  puts "I believe I didn't get your name. What is it?"
+  player = Player.new(gets.chomp)
+  game = Game.new(word_storage.word, player, player.name, image)
+  game.play
+end
+if option == 'load'
+  puts 'Sorry, but save states are a work in progress!!!'
+end
